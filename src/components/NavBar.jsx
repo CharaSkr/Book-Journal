47556e93c4d7d0532c463d930bb17d6cd4fd1e65
CharaSkr <chara.sk84@gmail.com>
2026-09-02 22:36:55 +0300

    test change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
       <nav>
         <ul className="flex gap-6">
           <li>
-            <Link to="/dashboard">Dashboard</Link>  
+            <Link to="/dashboard">Dashboard1</Link>  
           </li>
 
           <li>
