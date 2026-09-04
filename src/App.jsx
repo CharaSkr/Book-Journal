1f4b4c4a65d8a7eede10016e4fa163b275e2890a
CharaSkr <chara.sk84@gmail.com>
2026-09-04 22:02:51 +0300

    protected routes app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import MainLayout from './layouts/MainLayout'
-import LandingPage from './pages/LandingPage'
-import Dashboard from './pages/Dashboard'
-import Books from './pages/Books'
-import Statistics from './pages/Statistics'
-import Login from './pages/Login'
+import MainLayout from "./layouts/MainLayout";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import Books from "./pages/Books";
+import Statistics from "./pages/Statistics";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<LandingPage />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="books" element={<Books />} />
-        <Route path="statistics" element={<Statistics />} />
         <Route path="login" element={<Login />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="books" element={<Books />} />
+          <Route path="statistics" element={<Statistics />} />
+        </Route>
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
